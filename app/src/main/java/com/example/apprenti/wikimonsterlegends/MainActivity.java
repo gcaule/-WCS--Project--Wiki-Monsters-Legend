4695d0226adf2f37445b515c8c683bf70e7967ef
Germain Caule <g.caule31@outlook.fr>
2017-09-06 17:52:49 +0200

    Création page d'accueil

--- app/src/main/java/com/example/apprenti/wikimonsterlegends/MainActivity.java
+++ app/src/main/java/com/example/apprenti/wikimonsterlegends/MainActivity.java
@@ -1,15 +1,18 @@
 package com.example.apprenti.wikimonsterlegends;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 
 public class MainActivity extends ActionBarActivity {
 
     private ListView mListView;
-    private String[] prenoms = new String[]{
+    private String[] listeMonstres = new String[]{
             "Tyrannoking", "Rockilla", "Fire Lion", "Arch Knight", "Light Spirit", "Genie",
             "Metalsaur", "Panda", "Thunder Eagle", "Turtle"
     };
@@ -19,13 +22,20 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button retourAccueil = (Button) findViewById(R.id.button);
+        retourAccueil.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SelectMonsterDetails.class);
+                startActivity(intent);
+            }
+        });
+
         mListView = (ListView) findViewById(R.id.liste_deroulante);
 
-        //android.R.layout.simple_list_item_1 est une vue disponible de base dans le SDK android,
-        //Contenant une TextView avec comme identifiant "@android:id/text1"
-
         final ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
-                android.R.layout.simple_list_item_1, prenoms);
+                android.R.layout.simple_list_item_1, listeMonstres);
         mListView.setAdapter(adapter);
     }
 }
