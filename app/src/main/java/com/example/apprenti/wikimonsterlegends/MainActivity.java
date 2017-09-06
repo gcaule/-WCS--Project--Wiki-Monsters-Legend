47d80688d3b3851ee0a594af44d6d5d03d6628b6
Germain Caule <g.caule31@outlook.fr>
2017-09-06 17:25:08 +0200

    Création page d'accueil

--- app/src/main/java/com/example/apprenti/wikimonsterlegends/MainActivity.java
+++ app/src/main/java/com/example/apprenti/wikimonsterlegends/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.apprenti.wikimonsterlegends;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends ActionBarActivity {
+
+    private ListView mListView;
+    private String[] prenoms = new String[]{
+            "Tyrannoking", "Rockilla", "Fire Lion", "Arch Knight", "Light Spirit", "Genie",
+            "Metalsaur", "Panda", "Thunder Eagle", "Turtle"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mListView = (ListView) findViewById(R.id.liste_deroulante);
+
+        //android.R.layout.simple_list_item_1 est une vue disponible de base dans le SDK android,
+        //Contenant une TextView avec comme identifiant "@android:id/text1"
+
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, prenoms);
+        mListView.setAdapter(adapter);
     }
 }
