5313fa715a8504e46adfecbde575158a8fda0779
Nono <lombardo.nora@gmail.com>
2017-09-06 17:46:54 +0200

    Création de l'activity Monster Detail et création de l'évènement au clic sur un élément de la liste

---
+++ app/src/main/java/com/example/apprenti/wikimonsterlegends/SelectMonsterDetails.java
@@ -0,0 +1,12 @@
+package com.example.apprenti.wikimonsterlegends;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectMonsterDetails extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_activity);
+    }
+}
