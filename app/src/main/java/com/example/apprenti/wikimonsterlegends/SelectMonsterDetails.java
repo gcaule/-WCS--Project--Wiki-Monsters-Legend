42267a610f9b1d7f214b82448ace4d669d7344db
Nono <lombardo.nora@gmail.com>
2017-09-08 16:43:00 +0200

    Design page details et ajout bouton retour

--- app/src/main/java/com/example/apprenti/wikimonsterlegends/SelectMonsterDetails.java
+++ app/src/main/java/com/example/apprenti/wikimonsterlegends/SelectMonsterDetails.java
@@ -1,12 +1,28 @@
 package com.example.apprenti.wikimonsterlegends;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SelectMonsterDetails extends AppCompatActivity {
+
+    private Button back;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detail_activity);
+
+        Button back = (Button) findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SelectMonsterDetails.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
