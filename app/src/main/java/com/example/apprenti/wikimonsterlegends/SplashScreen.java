1e4fd0998614165785390db67db1751f132fa1da
Germain Caule <g.caule31@outlook.fr>
2017-09-15 10:08:31 +0200

    Ajout barre de chargement

--- app/src/main/java/com/example/apprenti/wikimonsterlegends/SplashScreen.java
+++ app/src/main/java/com/example/apprenti/wikimonsterlegends/SplashScreen.java
@@ -5,15 +5,42 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
+import android.widget.ProgressBar;
 
 public class SplashScreen extends Activity {
 
+    private int progressStatus = 0;
+    private ProgressBar progressBar;
+    private Handler handler = new Handler();
     private static int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        progressBar = findViewById(R.id.Bar1);
+
+        new Thread(new Runnable() {
+            public void run() {
+                while (progressStatus < 100) {
+                    progressStatus += 6.75;
+
+                    handler.post(new Runnable() {
+                        public void run() {
+                            progressBar.setProgress(progressStatus);
+                        }
+                    });
+
+                    try {
+
+                        Thread.sleep(200);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -25,6 +52,5 @@ public class SplashScreen extends Activity {
         }, SPLASH_TIME_OUT);
 
     }
-
 
 }
